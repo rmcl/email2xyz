dbbb6faeb00661ea70f72c6238e21e335a043aa9
Russell McLoughlin <russ.mcl@gmail.com>
2011-09-04 12:49:31 -0700

    Added variable to set from address.

--- config.php
+++ config.php
@@ -1,5 +1,7 @@
 <?php
+
 $base_url = "http://<insert url where the script lives>";
+$from = "share-robot@<somewheres>";
 $users = array("Russ" => "russ-great-email@gmail.com",
                "Bill" => "emailme@thespot.com");
 ?>
